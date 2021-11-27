1da9d52d1e110eb6b3da8fe5d57fb5d6b3b23b0b
Brayan Vera <brayanvera@Brayans-MacBook-Pro-2.local>
2021-11-27 17:17:33 -0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+let names = ['brayan','camila','valen'];
+
+names.forEach(element => {
+    console.log(element)
+});
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+    
+}
